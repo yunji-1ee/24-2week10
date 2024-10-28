25de334deec03f78ea12d3b7f925fb447332bed8
yunji-1ee <yunji011219@gmail.com>
2024-10-28 10:56:25 +0900

    aa

---
+++ App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import Component1 from './Component1';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         20241028 react oss class -윤지
+        </p>
+        <Component1></Component1>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
